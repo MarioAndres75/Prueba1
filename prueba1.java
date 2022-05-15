6595a1b3ffb2ddab2c8c60d6f2689704b9085f6f
Mario <ocampom1975@gmail.com>
2022-05-15 13:41:27 -0300

    modificacion de prueba

--- prueba1.java
+++ prueba1.java
@@ -4,7 +4,8 @@ public class prueba1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hola");
+System.out.println("acutalizado");
+System.out.println("probando");
 	}
 
 }
