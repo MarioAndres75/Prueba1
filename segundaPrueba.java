b3b51e88f62e6a1fb0fba45d6bf1ef633ad6c866
Mario <ocampom1975@gmail.com>
2022-05-15 14:04:11 -0300

    version final

---
+++ segundaPrueba.java
@@ -0,0 +1,10 @@
+package ar.edu.unlam.pb2.prueba;
+
+public class segundaPrueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("la ultima");
+	}
+
+}
